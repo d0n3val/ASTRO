e73ea0a831ac7db936930d1f111fa7e3d57fd324
Ric <d0n3val@gmail.com>
2017-03-30 13:45:45 +0200

    App closes when pressing X window button

--- ModuleInput.cpp
+++ ModuleInput.cpp
@@ -58,6 +58,11 @@ update_status ModuleInput::PreUpdate()
 	if(keyboard[SDL_SCANCODE_ESCAPE])
 		return update_status::UPDATE_STOP;
 
+	SDL_Event event;
+	while(SDL_PollEvent(&event))
+		if(event.type == SDL_QUIT)
+			return update_status::UPDATE_STOP;
+
 	return update_status::UPDATE_CONTINUE;
 }
 
